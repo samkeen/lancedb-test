891a6e645ff4ba55f64a2925fabb24c3be855f4f
Sam Keen <sam.sjk@gmail.com>
2024-02-21 20:41:27 -0800

    refactored code to simplify - for now gor rid of some of the larger one liners - able to load table with embeddings vs mock data

--- src/main.rs
+++ src/main.rs
@@ -16,32 +16,14 @@ use vectordb::{connect, Result, Table, TableRef};
 async fn main() -> Result<()> {
     let db = init_db().await?;
 
-
-
-    /// Try some embeddings
-    // let documents = vec![
-    //     "Hello World!",
-    //     "Another sentence",
-    //     "And one last sentence",
-    //     "This is a beer",
-    //     "the beer is good",
-    //     "weekends are too short",
-    //     "The cat is warm",
-    //     "Blankets are the best",
-    //     "Rust is tuff, but that's okay",
-    //     "Learn something new everyday",
-    //     "I wan to to be a builder",
-    // ];
-    // read in tests/fixtures/mobi-dick.txt then split all the lines of the document
     // into a Vec<&str>, remoing empty lines
     let documents = read_file_and_split_lines("tests/fixtures/mobi-dick.txt").unwrap();
 
-
-    let embeddings = match create_embeddings(documents) {
+    let embeddings = match create_embeddings(documents[0..1000].to_vec()) {
         Ok(embeddings) => {
             println!("Embeddings length: {}", embeddings.len()); // -> Embeddings length: 4
             println!("Embedding dimension: {}", embeddings[0].len()); // -> Embedding dimension: 384
-            // println!("Embedding: {:?}", embeddings[0]); // -> Embedding: [0.1, 0.2, 0.3, ...]
+                                                                      // println!("Embedding: {:?}", embeddings[0]); // -> Embedding: [0.1, 0.2, 0.3, ...]
             embeddings
         }
         Err(e) => {
@@ -49,21 +31,9 @@ async fn main() -> Result<()> {
         }
     };
 
-    // Convert embeddings to the required format
-    // let embeddings_iter = embeddings.iter().map(|embedding| Some(embedding.iter().map(Some).collect::<Vec<_>>()));
-    // Convert embeddings to the required format
-    let embeddings_iter = embeddings.iter().map(|embedding| Some(embedding.iter().map(|&value| Some(value.clone())).collect::<Vec<_>>()));
-
-
-
-
-
-
-
-
     println!("Initial table names> {:?}", db.table_names().await?);
-    let tbl = create_table(db.clone()).await?;
-    // print the number of records in the table
+    let _records = vec![vec![1.0f32; 128]; 1000];
+    let tbl = create_table(db.clone(), embeddings).await?;
     println!(
         "Number of records in the table> {}",
         tbl.count_rows(None).await?
@@ -88,7 +58,7 @@ async fn init_db() -> Result<Arc<dyn Connection>> {
 }
 
 fn drop_data_dir() {
-    if std::path::Path::new("data").exists() {
+    if Path::new("data").exists() {
         std::fs::remove_dir_all("data").unwrap();
     }
 }
@@ -97,19 +67,15 @@ fn drop_data_dir() {
 async fn open_with_existing_tbl() -> Result<()> {
     let uri = "data/sample-lancedb";
     let db = connect(uri).await?;
-    // --8<-- [start:open_with_existing_file]
     let _ = db
         .open_table_with_params("my_table", Default::default())
         .await
         .unwrap();
-    // --8<-- [end:open_with_existing_file]
     Ok(())
 }
 
-async fn create_table(db: Arc<dyn Connection>) -> Result<TableRef> {
-    // --8<-- [start:create_table]
-    const TOTAL: usize = 1000;
-    const DIM: usize = 128;
+async fn create_table(db: Arc<dyn Connection>, records: Vec<Vec<f32>>) -> Result<TableRef> {
+    let dimensions_count = records[0].len();
 
     let schema = Arc::new(Schema::new(vec![
         Field::new("id", DataType::Int32, false),
@@ -117,15 +83,13 @@ async fn create_table(db: Arc<dyn Connection>) -> Result<TableRef> {
             "vector",
             DataType::FixedSizeList(
                 Arc::new(Field::new("item", DataType::Float32, true)),
-                DIM as i32,
+                dimensions_count as i32,
             ),
             true,
         ),
     ]));
 
-    // Create a RecordBatch stream needed by `create_table`.
-    // this turns a vector into an iterator & .map(Ok) turns it into an iterator of Result<RecordBatch>
-    let records_iter = create_record_batch(TOTAL, DIM, schema.clone())
+    let records_iter = create_record_batch(records, schema.clone())
         .into_iter()
         .map(Ok);
     let batches = RecordBatchIterator::new(records_iter, schema.clone());
@@ -135,44 +99,56 @@ async fn create_table(db: Arc<dyn Connection>) -> Result<TableRef> {
         .await
         .unwrap();
 
-    // this turns a vector into an iterator & .map(Ok) turns it into an iterator of Result<RecordBatch>
-    let more_records_iter = create_record_batch(TOTAL, DIM, schema.clone())
-        .into_iter()
-        .map(Ok);
-    let more_batches = RecordBatchIterator::new(more_records_iter, schema.clone());
-
-    tbl.add(Box::new(more_batches), None).await.unwrap();
+    // let more_records = vec![vec![1.0; dimensions_count]; 1000];
+    // let more_records_iter = create_record_batch(more_records, schema.clone())
+    //     .into_iter()
+    //     .map(Ok);
+    // let more_batches = RecordBatchIterator::new(more_records_iter, schema.clone());
+    //
+    // tbl.add(Box::new(more_batches), None).await.unwrap();
 
     Ok(tbl)
 }
 
-fn create_record_batch(total: usize, dim: usize, schema: Arc<Schema>) -> Vec<RecordBatch> {
+///
+/// If TOTAL were 2, and DIM were 3, this creates the following:
+// [
+//     Some(
+//         [
+//             Some(1.0),
+//             Some(1.0),
+//             Some(1.0),
+//         ],
+//     ),
+//     Some(
+//         [
+//             Some(1.0),
+//             Some(1.0),
+//             Some(1.0),
+//         ],
+//     ),
+// ]
+fn wrap_in_option<T>(source: Vec<Vec<T>>) -> Vec<Option<Vec<Option<T>>>> {
+    source
+        .into_iter()
+        .map(|inner_vec| Some(inner_vec.into_iter().map(|item| Some(item)).collect()))
+        .collect()
+}
+
+fn create_record_batch(source: Vec<Vec<f32>>, schema: Arc<Schema>) -> Vec<RecordBatch> {
+    let total_records_count = source.len();
+    let dimensions_count = source[0].len();
+    let wrapped_source = wrap_in_option(source);
     vec![RecordBatch::try_new(
         schema.clone(),
         vec![
-            // this creates the id's (0 -> <TOTAL-1>)
-            Arc::new(Int32Array::from_iter_values(0..total as i32)),
-            // If TOTAL were 2, and DIM were 3, this creates the following:
-            // [
-            //     Some(
-            //         [
-            //             1.0,
-            //             1.0,
-            //             1.0,
-            //         ],
-            //     ),
-            //     Some(
-            //         [
-            //             1.0,
-            //             1.0,
-            //             1.0,
-            //         ],
-            //     ),
-            // ]
+            // This is the id field
+            Arc::new(Int32Array::from_iter_values(0..total_records_count as i32)),
+            // This is the vector field
             Arc::new(
                 FixedSizeListArray::from_iter_primitive::<Float32Type, _, _>(
-                    (0..total).map(|_| Some(vec![Some(1.0); dim])),
-                    dim as i32,
+                    wrapped_source,
+                    dimensions_count as i32,
                 ),
             ),
         ],
@@ -181,7 +157,6 @@ fn create_record_batch(total: usize, dim: usize, schema: Arc<Schema>) -> Vec<Rec
 }
 
 async fn create_empty_table(db: Arc<dyn Connection>) -> Result<TableRef> {
-    // --8<-- [start:create_empty_table]
     let schema = Arc::new(Schema::new(vec![
         Field::new("id", DataType::Int32, false),
         Field::new("item", DataType::Utf8, true),
@@ -189,22 +164,20 @@ async fn create_empty_table(db: Arc<dyn Connection>) -> Result<TableRef> {
     let batches = RecordBatchIterator::new(vec![], schema.clone());
     db.create_table("empty_table", Box::new(batches), None)
         .await
-    // --8<-- [end:create_empty_table]
 }
 
 async fn create_index(table: &dyn Table) -> Result<()> {
-    // --8<-- [start:create_index]
     table
         .create_index(&["vector"])
         .ivf_pq()
         .num_partitions(8)
         .build()
         .await
-    // --8<-- [end:create_index]
 }
 
+// @TODO getting this error: Error: Store { message: "No vector column found to create index" }
+// @TODO fix this: instead of [1.0; 128], use the embedding of one of the lines from the document
 async fn search(table: &dyn Table) -> Result<Vec<RecordBatch>> {
-    // --8<-- [start:search]
     println!("Searching for vectors similar to [1.0; 128]...");
     Ok(table
         .search(&[1.0; 128])
@@ -213,12 +186,11 @@ async fn search(table: &dyn Table) -> Result<Vec<RecordBatch>> {
         .await?
         .try_collect::<Vec<_>>()
         .await?)
-    // --8<-- [end:search]
 }
 
 fn read_file_and_split_lines<P>(filename: P) -> io::Result<Vec<String>>
-    where
-        P: AsRef<Path>,
+where
+    P: AsRef<Path>,
 {
     let file = File::open(filename)?;
     let reader = BufReader::new(file);
